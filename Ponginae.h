3de411b246b45a7557f5ca07fba6a7586674318d
farrukh <f.buriyev@newuu.uz>
2025-02-16 11:50:58 +0500

    Create Ponginae.h

---
+++ Ponginae.h
@@ -0,0 +1,12 @@
+//
+// Created by qwerty on 06.02.2025.
+//
+
+#ifndef PONGINAE_H
+#define PONGINAE_H
+#include "Hominidae.h"
+#include "Homininae.h"
+
+class Ponginae : Homininae {};
+
+#endif //PONGINAE_H
